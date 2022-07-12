c03a5117beff9fde992e441c0bf9aeff90b9f0a6
NewChap2022 <irisyangshu@gmail.com>
2022-07-12 19:22:40 -0400

    create database

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS employees_info;
+CREATE DATABASE employees_info;
+USE employees_info;
---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (name)
+VALUES
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Sales Lead', 100000, 1),
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2),
+('Software Engineer', 120000, 2),
+('Account Manager', 160000, 3),
+('Accountant', 125000, 3);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
+('Andy', 'Bill', 1, NULL),
+('Ben', 'Aniston', 2, 1);
+
