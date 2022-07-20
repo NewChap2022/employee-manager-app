260a1352f7d55eca6a2d9691d383e55d57460e8b
NewChap2022 <irisyangshu@gmail.com>
2022-07-20 19:22:43 -0400

    update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -11,14 +11,14 @@ CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL REFERENCES departments(id)
+    department_id INT NOT NULL FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL REFERENCES roles(id),
+    role_id INT NOT NULL FOREIGN KEY (role_id) REFERENCES roles(id),
     manager_id INT,
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
