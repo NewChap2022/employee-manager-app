8d9c1fe3a23cc99c1ba8d8a8fbac59fd9bd33b20
NewChap2022 <irisyangshu@gmail.com>
2022-07-20 19:36:41 -0400

    fix bug

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
+    department_id INT NOT NULL FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE NO ACTION
 );
 
 CREATE TABLE employees (
